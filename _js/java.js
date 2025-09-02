1780dc1e2aae32ffc8514718a961a0a61e7dcf18
GideaoDSantos <gideao.d.santos.aluno.senai.br>
2025-09-02 16:41:37 -0300

    js em andamento

--- _js/java.js
+++ _js/java.js
@@ -1,2 +1,22 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe")
+
+
+const jump = () => {// "=>" serve como function
+    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 500);
+
+}
+
+const loop = setInterval(() => {
+    console.log('loop');
+})
+
+const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = "none";
+}
